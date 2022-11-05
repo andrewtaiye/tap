3fdb75940e7ed8e533f18d01e83b8415198ba099
Andrew Tai <andrewtai.ye@gmail.com>
2022-11-06 00:25:27 +0800

    Added Assessments Page

---
+++ src/pages/Assessments.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Assessments = () => {
+  return (
+    <>
+      <Header />
+      Assessments
+    </>
+  );
+};
+
+export default Assessments;
