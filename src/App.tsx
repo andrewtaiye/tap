8556ef2e84a9f5a3065c15a673b021fdd4c2795a
Andrew Tai <andrewtai.ye@gmail.com>
2022-11-05 19:22:01 +0800

    Register Layout Complete

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import "./styles/styles.css";
 import "./styles/utility.css";
 
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -5,7 +5,7 @@ import { ReactComponent as Logo } from "../assets/logos/image.svg";
 import InputFieldWithLegend from "../components/InputFieldWithLegend";
 import Button from "../components/Button";
 
-const Login = () => {
+const Register = () => {
   return (
     <div className="login-register__container">
       <Logo style={{ height: "350px" }} className="mb-6" />
@@ -15,41 +15,49 @@ const Login = () => {
           type="text"
           className="input__login fs-32 mb-4"
           placeholder="Username"
+          warning={true}
+        />
+        <InputFieldWithLegend
+          type="password"
+          className="input__login fs-32 mb-4"
+          placeholder="Password"
+          warning={true}
         />
         <InputFieldWithLegend
           type="password"
           className="input__login fs-32 mb-1"
-          placeholder="Password"
+          placeholder="Confirm Password"
+          warning={true}
         />
         <p
           className="error fs-24 mb-8"
           style={{ alignSelf: "flex-start", textAlign: "left" }}
         >
-          Error: Invalid Username or Password
+          Error: Passwords do not match
         </p>
       </form>
       <div className="row gap-32">
-        <Button
-          mode="active"
-          type="submit"
-          className="fs-32"
-          style={{ width: "136px" }}
-        >
-          Login
-        </Button>
-        <NavLink to="/register">
+        <NavLink to="/login">
           <Button
-            mode="outline"
-            type="button"
+            mode="active"
+            type="submit"
             className="fs-32"
             style={{ width: "136px" }}
           >
-            Register
+            Login
           </Button>
         </NavLink>
+        <Button
+          mode="outline"
+          type="button"
+          className="fs-32"
+          style={{ width: "136px" }}
+        >
+          Register
+        </Button>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Register;
