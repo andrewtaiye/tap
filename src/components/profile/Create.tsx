f3cab0d88db4bee9498ee75275d7b6a34601ffa7
Andrew Tai <andrewtai.ye@gmail.com>
2022-11-06 01:13:01 +0800

    Removed Console Log

--- src/components/profile/Create.tsx
+++ src/components/profile/Create.tsx
@@ -17,7 +17,6 @@ const Create = () => {
   const [errorMessage, setErrorMessage] = useState("");
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
-    console.log({ data });
     if (
       !data["salutation"] ||
       !data["full name"] ||
