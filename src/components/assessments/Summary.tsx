b58a83c79ec205984bf7fee75f5b8acb81565ba0
Andrew Tai <andrewtai.ye@gmail.com>
2022-11-15 23:20:46 +0800

    Added basic line chart

--- src/components/assessments/Summary.tsx
+++ src/components/assessments/Summary.tsx
@@ -1,4 +1,6 @@
-import React, { useContext } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useEffect, useRef, useState } from "react";
+import * as d3 from "d3";
 
 import GlobalVariables from "../../context/GlobalVariables";
 
@@ -10,11 +12,65 @@ interface Props {
 }
 
 const Summary = (props: Props) => {
-  const { userPositions } = useContext(GlobalVariables);
+  const { userPositions, positionAssessments } = useContext(GlobalVariables);
+  const [grades, setGrades] = useState<(number | undefined)[]>([]);
+  const lineChart: any = useRef();
 
   const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     props.setSelectedPosition(event.target.value);
   };
+
+  useEffect(() => {
+    if (positionAssessments) {
+      const gradeArray = [];
+
+      for (const assessment of positionAssessments) {
+        gradeArray.push(assessment.grade);
+      }
+
+      setGrades(gradeArray);
+    }
+  }, []);
+
+  useEffect(() => {
+    // set up svg
+    const width = 700;
+    const height = 400;
+    const svg = d3
+      .select(lineChart.current)
+      .attr("width", width)
+      .attr("height", height)
+      .style("overflow", "visible");
+
+    // set up scaling
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, positionAssessments!.length - 1])
+      .range([0, width]);
+    const yScale: any = d3.scaleLinear().domain([0, 100]).range([height, 0]);
+    const generateScaledLine = d3
+      .line()
+      .x((d, i) => xScale(i))
+      .y(yScale);
+
+    // set up axes
+    const xAxis = d3
+      .axisBottom(xScale)
+      .ticks(grades.length)
+      .tickFormat((i: any) => i + 1);
+    const yAxis = d3.axisLeft(yScale).ticks(5);
+    svg.append("g").call(xAxis).attr("transform", `translate(0,${height})`);
+    svg.append("g").call(yAxis);
+
+    // set up data
+    svg
+      .selectAll(".line")
+      .data([grades])
+      .join("path")
+      .attr("d", (d: any) => generateScaledLine(d))
+      .attr("fill", "none")
+      .attr("stroke", "black");
+  }, [positionAssessments]);
 
   return (
     <>
@@ -57,7 +113,7 @@ const Summary = (props: Props) => {
             marginInline: "auto",
           }}
         >
-          Assessment Grade Chart
+          <svg ref={lineChart}></svg>
         </div>
       )}
     </>
