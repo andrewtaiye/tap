0283fbb3fc9c704bb5d959d34c3788b93bbda0b1
Andrew Tai <andrewtai.ye@gmail.com>
2022-11-25 21:22:23 +0800

    Added: UseEffect for Overview to Get Trainees

--- src/components/instructor/Overview.tsx
+++ src/components/instructor/Overview.tsx
@@ -1,6 +1,22 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useEffect } from "react";
+
+import GlobalVariables from "../../context/GlobalVariables";
+import { fetchCall } from "../generic/utility";
 
 const Overview = () => {
+  const { accessToken } = useContext(GlobalVariables);
+
+  useEffect(() => {
+    (async () => {
+      const url =
+        process.env.REACT_APP_API_ENDPOINT + `instructor/get/trainees`;
+      const res = await fetchCall(url, accessToken.current);
+
+      console.log(res);
+    })();
+  }, []);
+
   return <div>Overview</div>;
 };
 
